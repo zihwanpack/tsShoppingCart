8bd88bc1101cc6310689869b4e7ebd9f67478fa8
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2024-01-16 21:34:55 +0900

    중간 저장

---
+++ src/context/ShoppingCartContext.tsx
@@ -0,0 +1,54 @@
+import { ReactNode, createContext, useContext } from "react";
+
+type ShoppingCartProviderProps = {
+  children: ReactNode;
+};
+
+type ShoppingCartContext = {
+  getItemQuantity: (id: number) => number;
+  increaseCartQuantity: (id: number) => void;
+  decreaseCartQuantity: (id: number) => void;
+  removeFromCart: (id: number) => void;
+};
+
+type CartItem = {
+  id: number;
+  quantity: number;
+};
+
+const ShoppingCartContext = createContext({} as ShoppingCartContext);
+
+export function useShoppingCart() {
+  return useContext(ShoppingCartContext);
+}
+
+export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  function getItemQuantity(id: number) {
+    return cartItems.find((item) => item.id === id)?.quantity || 0;
+  }
+  function increaseCartQuantity(id: number) {
+    setCartItems((currItems) => {
+      if (currItems.find((item) => item.id === id) == null) {
+        // 찾는 item이 없을 경우 만들어준다.
+        return [...currItems, { id, quantity: 1 }];
+      } else {
+        return currItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  }
+  return (
+    <ShoppingCartContext.Provider
+      value={{ getItemQuantity, increaseCartQuantity }}
+    >
+      {children}
+    </ShoppingCartContext.Provider>
+  );
+}
